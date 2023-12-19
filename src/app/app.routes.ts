13a2b3cf6bb4e516d6460efeeea853a2f858013e
Pratik <sahoopratik22@gmail.com>
2023-12-19 23:30:25 +0530

    login page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ import { BlogComponent } from './blog/blog.component';
 export const routes: Routes = [
     { path:'', component:BlogComponent, 
        loadChildren: ()=> import('./blog/blog.route').then((m)=>m.BLOG_ROUTE),
-}
+    },
+    {
+        path:'auth',
+        loadChildren: () => import('./auth/auth.route'),
+    },
 ];
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatFormFieldModule, MatButtonModule, MatInputModule, MatCardModule,RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
